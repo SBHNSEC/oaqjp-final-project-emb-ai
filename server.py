5f0906d9f5005700b39e6a9e18fe5665e43878c2
Somasree Bhadra <somasree.bhadra@nsec.ac.in>
2025-08-21 13:57:07 -0400

    Task 6

---
+++ server.py
@@ -0,0 +1,26 @@
+from EmotionDetection import emotion_detector
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/emotionDetector', methods=['GET'])
+def detect_emotion():
+    textToAnalyze = request.args.get("textToAnalyze") #request.args.get("textToAnalyze")
+    result = emotion_detector(textToAnalyze)
+    response_text = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}"
+        f"'disgust': {result['disgust']}"
+        f"'fear': {result['fear']}"
+        f"'joy': {result['joy']}"
+        f"'sadness': {result['sadness']}"
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+    return response_text
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
