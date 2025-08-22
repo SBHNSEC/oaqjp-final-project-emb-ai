9aba7f4011e097b980a126b550702ffb036c0298
Somasree Bhadra <somasree.bhadra@nsec.ac.in>
2025-08-22 04:52:00 -0400

    Task 8

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
-from EmotionDetection import emotion_detector
+"""
+Flask server used as backend server to NLP - Emotion Detection.
+This server appliaction uses the Watson NLP Library to score 
+the emotions in the statement and finally reveal the dominant 
+emotion among all the scored emotions.
+"""
+
 from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Renders the Homepage of the NLP - Emotion Detection Application"""
     return render_template("index.html")
 
 @app.route('/emotionDetector', methods=['GET'])
 def detect_emotion():
-    textToAnalyze = request.args.get("textToAnalyze")
-    result = emotion_detector(textToAnalyze)
+    """Detects the Emotions of the User Input Statement"""
+    text_to_analyze = request.args.get("textToAnalyze")
+    result = emotion_detector(text_to_analyze)
 
     if result.get('dominant_emotion') is None:
         return "Invalid text! Please try again!."
